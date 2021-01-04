5ab0221ffe4ec1e910718a9f8aad9604383fc4e2
Pawel Suchta <pawek35@gmail.com>
2021-01-04 03:44:56 +0100

    Adds implementation of SiteLogParser Service. Changes expected result's order in SiteLogParser Spec

--- services/site_log_parser.rb
+++ services/site_log_parser.rb
@@ -1,10 +1,44 @@
 # frozen_string_literal: true
 
+require_relative '../models/site'
+
 module Services
   # A service which returns site names, sorted by the most visited ones
   class SiteLogParser
-    def initialize; end
+    attr_writer :sort_strategy
+    attr_reader :result
 
-    def call(_file_path); end
+    def initialize(sort_strategy)
+      @sort_strategy = sort_strategy
+      @site_collection = {}
+      @result = []
+    end
+
+    def call(file_path)
+      create_site_hash(file_path)
+      @result = sites_visit_count
+    end
+
+    def sort
+      sort_strategy(result)
+    end
+
+    private
+
+    attr_reader :site_collection
+
+    # Creates a Hash. Key is site_path and Value is Site model.
+    def create_site_hash(file_path)
+      File.readlines(file_path).each do |line|
+        site_path, ip = line.split
+        site_collection[site_path] ||= Models::Site.new(site_path)
+        site_collection[site_path].visit(ip)
+      end
+    end
+
+    # Returns an array with visit count per each site
+    def sites_visit_count
+      site_collection.values.map(&:visit_print)
+    end
   end
 end
--- spec/services/site_log_parser_spec.rb
+++ spec/services/site_log_parser_spec.rb
@@ -4,7 +4,7 @@ require 'tempfile'
 require_relative '../../services/site_log_parser'
 
 describe Services::SiteLogParser do
-  subject(:service) { described_class.new }
+  subject(:service) { described_class.new(nil) }
 
   let(:test_log_file) do
     Tempfile.new('log').tap do |file|
@@ -19,23 +19,23 @@ describe Services::SiteLogParser do
     context 'when log is not empty' do
       let(:lines) do
         [
-          ['/page1', '000.000.000.001'],
+          ['/page4', '000.000.000.004'],
           ['/page2', '000.000.000.001'],
           ['/page1', '000.000.000.002'],
           ['/page2', '000.000.000.002'],
           ['/page1', '000.000.000.003'],
           ['/page3', '000.000.000.003'],
-          ['/page4', '000.000.000.004']
+          ['/page1', '000.000.000.001']
         ]
       end
 
       it 'returns array with site counts, sorted by visit count' do
         expect(service.call(test_log_file.path)).to eq(
           [
-            '/page1 3',
-            '/page2 2',
-            '/page3 1',
-            '/page4 1'
+            '/page1 3 visits',
+            '/page2 2 visits',
+            '/page3 1 visits',
+            '/page4 1 visits'
           ]
         )
       end
